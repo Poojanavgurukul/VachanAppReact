63822e39d7ebeda37907bc2e9d86221120d56309
Pooja.Saini <pooja.saini@bridgeconn.com>
2022-07-13 15:38:29 +0530

    about us ui changes

--- app/screens/About/styles.js
+++ app/screens/About/styles.js
@@ -8,50 +8,51 @@ export const styles = (colorFile, sizeFile) => {
       backgroundColor: colorFile.backgroundColor,
     },
     textStyle: {
-      fontSize: sizeFile.contentText,
       color: colorFile.textColor,
-      lineHeight: sizeFile.lineHeight,
+      fontSize: sizeFile.contentText,
+      textAlign: "justify",
     },
     textContainer: {
       margin: 16,
       padding: 8,
     },
-    // featureList: {
-    //     fontSize: sizeFile.contentText,
-    //     color: colorFile.textColor,
-    //     fontWeight: "bold"
-    // },
-    // italicText: {
-    //     fontWeight: 'bold', fontStyle: 'italic'
-    // },
     boldText: {
       fontWeight: "bold",
       color: colorFile.textColor,
+      fontSize: sizeFile.titleText,
     },
     linkText: {
       color: Color.Red,
       textDecorationLine: "underline",
       fontSize: sizeFile.contentText,
     },
-    // featureView: {
-    //     flexDirection: 'row'
-    // },
-    TitleText: {
-      paddingTop: 8,
+    titleText: {
+      paddingTop: 6,
       fontSize: sizeFile.titleText,
       color: colorFile.sectionHeading,
-      lineHeight: sizeFile.lineHeight,
     },
     bulletIcon: {
-      fontSize: 26,
-      color: colorFile.iconColor,
+      fontSize: sizeFile.contentText,
+      color: colorFile.textColor,
       lineHeight: sizeFile.lineHeight,
     },
-    releaseNoteText: {
+    bulletText: {
+      flex: 1,
+      paddingLeft: 10,
+      color: colorFile.textColor,
+      fontSize: sizeFile.contentText,
+      lineHeight: sizeFile.lineHeight,
+    },
+    list: {
+      flex: 1,
+      paddingLeft: 5,
+    },
+    heading: {
       paddingTop: 8,
       lineHeight: sizeFile.lineHeight,
-      fontSize: sizeFile.titleText,
+      fontSize: sizeFile.contentText,
       color: colorFile.textColor,
+      fontWeight: "bold",
     },
   });
 };
